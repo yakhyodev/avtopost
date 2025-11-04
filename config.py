2ff3e1e34caeb2ec196a6dd4d185cf44cc527b7a
yakhyodev <yakhyo.dev@gmail.com>
2025-11-04 15:16:43 +0500

    Refactor config.py for clarity and efficiency

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 import os
 from dotenv import load_dotenv
 
-# Faqat lokal ishga tushirish uchun .env ni yuklaymiz. Renderda bu shart emas.
+# Lokal ishga tushirish uchun .env ni yuklaymiz
 load_dotenv()
 
 # --- BOT UCHUN ASOSIY KONFIGURATSIYA ---
@@ -16,12 +16,8 @@ admin_id_str = os.getenv("ADMIN_ID")
 
 # Stringni qayta ishlash va List[int] ga aylantirish
 if admin_id_str:
-    # 1. Vergul bo'yicha ajratish
-    # 2. Har bir ID ning bosh va oxiridagi bo'shliqlarni olib tashlash (strip())
-    # 3. Uni butun songa (int) aylantirish
+    # Stringni vergul bo'yicha ajratish va har bir elementni intga aylantirish
     ADMIN_ID = [int(id.strip()) for id in admin_id_str.split(',')]
 else:
-    # Agar hech qanday ID kiritilmasa (yoki .env da topilmasa), bo'sh ro'yxat qaytariladi
-    ADMIN_ID = [] 
-
-# ADMIN_ID endi barcha admin ID raqamlarini o'z ichiga olgan Python List'i [12345, 98765]
+    # Agar ID topilmasa, bo'sh ro'yxat qaytariladi
+    ADMIN_ID = []
